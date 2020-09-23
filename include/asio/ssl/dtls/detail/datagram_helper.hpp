7b8277a3327d17c2dcbe7b0fc0c15519f0c3773a
Stefan Damm <stefan@autama.de>
2020-09-24 01:55:38 +0200

    Fix client timeout logic

--- include/asio/ssl/dtls/detail/datagram_helper.hpp
+++ include/asio/ssl/dtls/detail/datagram_helper.hpp
@@ -18,6 +18,8 @@
 #include "asio/error_code.hpp"
 #include "asio/steady_timer.hpp"
 
+#include "asio/ssl/dtls/detail/macro_helper.hpp"
+
 #include "asio/detail/push_options.hpp"
 #endif // ASIO_DTLS_USE_BOOST
 #include <functional>
@@ -135,42 +137,65 @@ public:
 
   async_datagram_receive_timeout(SocketType& socket)
     : socket_(socket)
-    , timer_(socket.get_executor())
+    , timer_(new asio::steady_timer(socket.get_executor()))
     , timeout_(new asio::steady_timer::duration(chrono::seconds(1)))
+    , timedout_(new bool(false))
   {
-    timer_.expires_after(asio::steady_timer::duration::max());
+    timer_->expires_after(asio::steady_timer::duration::max());
   }
 
   async_datagram_receive_timeout(const async_datagram_receive_timeout& other)
       : socket_(other.socket_)
-      , timer_(socket_.get_executor())
+      , timer_(other.timer_)
       , timeout_(other.timeout_)
+      , timedout_(other.timedout_)
   {
-      timer_.expires_after(asio::steady_timer::duration::max());
   }
 
   template <typename Buffer, typename CallBack>
-  void operator()(const Buffer& buffer, ASIO_DTLS_MOVE_ARG(CallBack) cb)
+  void operator()(const Buffer& buffer, CallBack cb)
   {
+    asio::detail::shared_ptr<asio::steady_timer> timer(timer_);
+
+    std::function<void(asio::error_code, std::size_t)> tmp(cb);
+
+    asio::detail::shared_ptr<bool> timedout(timedout_);
     socket_.async_receive(buffer, message_flags(),
-                          ASIO_DTLS_MOVE_CAST(CallBack)(cb));
+      [tmp, timer, timedout](asio::error_code ec, std::size_t size)
+      {
+        timer->cancel();
+        if(*timedout)
+        {
+          asio::error_code ec(asio::error::timed_out, asio::error::system_category);
+          tmp(ec, size);
+        }
+        else
+        {
+          tmp(ec, size);
+        }
+      });
 
     SocketType &socket = socket_;
-    timer_.async_wait([&socket](const asio::error_code &ec)
+    timer_->expires_after(*timeout_);
+    timer_->async_wait([&socket, timedout](const asio::error_code &ec)
     {
         if(!ec)
         {
+            *timedout = true;
             socket.close();
         }
     });
+  }
 
-    timer_.expires_after(*timeout_);
+  ~async_datagram_receive_timeout()
+  {
   }
 
 private:
   SocketType& socket_;
-  asio::steady_timer timer_;
+  asio::detail::shared_ptr<asio::steady_timer> timer_;
   asio::detail::shared_ptr<asio::steady_timer::duration > timeout_;
+  asio::detail::shared_ptr<bool> timedout_;
 };
 
 template <typename SocketType>
